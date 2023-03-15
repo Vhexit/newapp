7ddaebc8fe90a40537f2a8fbcad8ba60ee10ad15
yeah1tsmark <marcwafula635@gmail.com>
2023-03-15 13:05:14 +0300

    new

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,49 @@
+fun main(args: Array<String>) {
+    //Integer or Int
+    var ages:Int = 30
+
+    //Strings
+    var name = "Muraya"
+
+    //Double
+    var temperature = 36.51
+
+    //Boolean true or false
+    var Status = false
+
+    // float
+    var point:Float = 8.5F
+
+    // Convert double to float
+    var marks = 46.587F
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/kotlin/school.kt
+++ src/main/kotlin/school.kt
@@ -1,6 +1,27 @@
 fun main(args: Array<String>) {
 
-    print("Enter marks")
+    print("Enter Student Marks")
+
+    var marks:Int = readLine()!!.toInt()
+
+    if (marks >80){
+        println("A")
+        print("Congratulations")
+    }
+    else if(marks >70){
+        println("A-")
+        print("Almost there")
+        }
+
+    else if (marks >60){
+        println("B")
+        print("Trail")
+    }
+    else {
+        println("E")
+        print("fail")
+    }
+
 
 
 
