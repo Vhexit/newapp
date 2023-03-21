be0e8271a5107dd8c1926426fff7c81d6fb0a05e
yeah1tsmark <marcwafula635@gmail.com>
2023-03-21 12:40:18 +0300

    new

---
+++ src/main/kotlin/exfunction.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+
+multiplication()
+addition()
+}
+
+fun multiplication (){
+
+    var x = 20
+    var y = 100
+    var result = x * y
+    println(result)
+}
+
+fun addition(){
+    var x = 120
+    var y = 80
+    var result = x + y
+    println(result)
+}
---
+++ src/main/kotlin/functions.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+sayhello()
+displaymessage()
+
+
+
+}
+
+fun sayhello(){
+    println("Say hello guys")
+}
+
+fun displaymessage(){
+    println("Today is Tuesday")
+}
